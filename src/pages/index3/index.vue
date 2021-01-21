d128af83579703c13d873b62bc44a3c97b4270de
jake99 <1427170192@qq.com>
2021-01-21 16:35:22 +0800

    微信小程序的第二次提交

--- src/pages/index3/index.vue
+++ src/pages/index3/index.vue
@@ -2,6 +2,8 @@
   <view class="content">
     <!-- 2 使用数据{{msg}} -->
     <view>{{ msg }}</view>
+    
+    
   </view>
 </template>
 
@@ -12,6 +14,16 @@ export default {
     return {
       //存放数据
       msg: "黑马程序员-广州中心",
+      //数字
+      money:10000,
+      //bool类型
+      isRich:false,
+      //对象
+      person:{
+        name:"孙悟空",
+        skill:"72变",
+
+      }
     };
   },
 };
